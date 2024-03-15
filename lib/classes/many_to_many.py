3496539c20ffc129c20694413ceb5336dab27fd2
Michael <michaelmwendwamumo@gmail.com>
2024-03-15 14:24:45 +0300

    Progress made

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,6 +1,18 @@
 class Game:
+
+    all = []
+
     def __init__(self, title):
-        self.title = title
+        self._title = title
+        Game.all.append(self)
+    @property
+    def title(self):
+        if isinstance(self._title, str) and len(self._title) > 0:
+            return self._title
+    @title.setter
+    def title(self, title):
+        if hasattr(Game, 'title'):
+            print('Cannot change title')
 
     def results(self):
         pass
@@ -12,8 +24,21 @@ class Game:
         pass
 
 class Player:
+
+    all = []
+
     def __init__(self, username):
-        self.username = username
+        self._username = username
+        Player.all.append(self)
+    @property
+    def username(self):
+        if isinstance(self._username, str) and 2 <= len(self._username) <= 16:
+            return self._username
+        
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
 
     def results(self):
         pass
@@ -28,7 +53,30 @@ class Player:
         pass
 
 class Result:
+
+    all = []
+
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        if isinstance(self.score, int) and 1 <= self.score <= 5000:
+            return self.score
+    @score.setter
+    def score(self, score):
+        if hasattr(Result, 'score'):
+            print('Cannot change score')
+
+    @property
+    def player(self):
+        if isinstance(self.player, Player):
+            return self.player
+
+    @property
+    def game(self):
+        if isinstance(self.game, Game):
+            return self.game
